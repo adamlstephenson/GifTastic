3e2e92d509c48e18456da8a94aa3e0f69411a77c
Adam Stephenson <astephensonx@gmail.com>
2018-03-23 23:24:59 -0500

    all buttons displaying

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,26 +1,25 @@
 $("document").ready(function() {
 
-    console.log('hello');
-
 var topics = ['vikings', 'packers', 'bears', 'lions', 'eagles', 'cowboys', 'redskins', 'giants', 'saints', 'panthers', 'falcons', 'bucanneers', 'cardinals', '49ers', 'seahawks', 'rams'];
 var searchTeam;
 var searchTerm;
-for(i = 0; i < topics.length; i++) {
-    
-    searchTeam = topics[i].toString();
+
+function renderButtons() {
+for (i = 0; i < topics.length; i++) {
+
+    teamName = topics[i];
 
     var buttons = $("<button>");
     buttons.addClass("team");
-    buttons.attr("data-name", topics[i]);
-    buttons.text(topics[i]);
-    $("#gifbuttons").html(buttons);
-    
+    buttons.attr("data-name", teamName);
+    buttons.text(teamName);
+    $("#gifbuttons").append(buttons);
+    console.log(teamName);
+}
 }
 
-console.log(searchTeam);
-
 queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=RgOKKocZs8kcyzsU7F1E4hNZms3pjHFu&limit=10";
-queryURL += "&q=" + searchTeam;
+queryURL += "&q=" + searchTerm;
 
 
 $.ajax({
@@ -32,6 +31,8 @@ $.ajax({
 
 
   })
+
+  renderButtons();
 
 
 
